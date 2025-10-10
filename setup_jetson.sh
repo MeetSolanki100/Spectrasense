59d0f3d77ef9f2891516bcdef33008fcfe68d2d1
kabir-999 <kabirmathur05@gmail.com>
2025-10-10 16:42:56 +0530

    Updated setup_jetson.sh

--- setup_jetson.sh
+++ setup_jetson.sh
@@ -13,32 +13,38 @@ echo "[INFO] Installing system-level dependencies..."
 sudo apt-get update
 sudo apt-get install -y build-essential cmake libopenblas-dev libjpeg-dev libpython3-dev python3-pip
 
-# 2. Install PyTorch and TorchVision
-echo "[INFO] Installing PyTorch and TorchVision for Jetson..."
-# These URLs are for JetPack 4.x. You may need to find different wheels for other versions.
-# See: https://forums.developer.nvidia.com/t/pytorch-for-jetson/72048
-wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl
-wget https://nvidia.box.com/shared/static/3dhrj9yulw06k23d8m62i8a5s7i5xht8.whl -O torchvision-0.9.0-cp36-cp36m-linux_aarch64.whl
+# 2. Install PyTorch
+echo "[INFO] Installing PyTorch v1.10.0 for Jetson..."
+# URL for PyTorch 1.10.0 for JetPack 4.6.x (Python 3.6)
+# For other versions, see: https://forums.developer.nvidia.com/t/pytorch-for-jetson/72048
+wget https://developer.download.nvidia.com/compute/redist/jp/v461/pytorch/torch-1.10.0-cp36-cp36m-linux_aarch64.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 
 sudo apt-get install -y libopenblas-base libopenmpi-dev
 pip3 install Cython
-pip3 install numpy torch-1.8.0-cp36-cp36m-linux_aarch64.whl
-pip3 install torchvision-0.9.0-cp36-cp36m-linux_aarch64.whl
+pip3 install numpy torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+rm torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 
-# Clean up downloaded wheel files
-rm torch-1.8.0-cp36-cp36m-linux_aarch64.whl torchvision-0.9.0-cp36-cp36m-linux_aarch64.whl
+# 3. Install TorchVision
+echo "[INFO] Building and installing TorchVision v0.11.1 from source..."
+sudo apt-get install -y libjpeg-dev zlib1g-dev
+git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
+cd torchvision
+export BUILD_VERSION=0.11.1
+python3 setup.py install --user
+cd ..
+rm -rf torchvision
 
-# 3. Install dlib and face_recognition
+# 4. Install dlib and face_recognition
 echo "[INFO] Installing dlib and face_recognition..."
 # This may take a very long time as dlib is built from source.
 pip3 install dlib
 pip3 install face_recognition
 
-# 4. Install OpenCV
+# 5. Install OpenCV
 echo "[INFO] Installing OpenCV..."
 pip3 install opencv-python
 
-# 5. Install remaining packages from requirements.txt
+# 6. Install remaining packages from requirements.txt
 echo "[INFO] Installing remaining Python packages..."
 pip3 install -r requirements.txt
 
